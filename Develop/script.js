87f21bfa27b5ea52ffd639304828235b34385adc
Victoria David <victoria@thedavids.us>
2022-06-28 13:07:33 -0400

    working prompt savepoint

--- Develop/script.js
+++ Develop/script.js
@@ -9,54 +9,83 @@
 6. Display password
 */
 
-var password =
-[
-  {
-    randomLower: function()
-    {
-      var confirmLower = window.confirm("Do you want lowercase letters in your password?");
-      if (confirmLower)
-      {
-        var lowercase = "abcdefghijklmnopqrstuvwxyz";
-        return lowercase(Math.floor(Math.random() * lowercase.length));
-      }
-    }
-  },
-  {
-    randomUpper: function()
-    {
-      var confirmUpper = window.confirm("Do you want uppercase letters in your password?");
-      if (confirmUpper)
-      {
-        var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        return uppercase(Math.floor(Math.random() * uppercase.length));
-      }
-    }
-  },
-  {
-    randomNumber: function()
-    {
-      var number = "0123456789";
-      return number(Math.floor(Math.random() * number.length));
-    }
-  },
-  {
-    randomSymbol: function()
-    {
-      var symbol = "!@#$%^&*=+./";
-      return symbol(Math.floor(Math.random() * symbol.length));
-    }
-  }
-]
-
-var generatePassword = function()
+//randomizer functions to call later (?)
+const randomLower = function()
 {
-  var passwordLength = window.prompt("How many characters will your password have?")
-  if (passwordLength >= 8 && passwordLength <= 128)
-  {
-    passwordLength = parseInt(passwordLength);
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  return lowercase(Math.floor(Math.random() * lowercase.length));
+};
 
-  }
+const randomUpper = function()
+{
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return uppercase(Math.floor(Math.random() * uppercase.length));
+};
+
+const randomNumber = function()
+{
+  var number = "0123456789";
+  return number(Math.floor(Math.random() * number.length));
+};
+
+const randomSymbol = function()
+{
+  var symbol = " `~!@#$%^&*/.,";
+  return symbol(Math.floor(Math.random() * symbol.length));
+};
+
+const generatePassword = function()
+{
+  //user input
+  let lengthChoice = window.prompt("How long would you like your password?");
+  //convert string to interger
+  lengthChoice = parseInt(lengthChoice);
+    if (lengthChoice >= 8 && lengthChoice <= 128)
+    {
+      window.confirm("Your password will be " +lengthChoice+ " characters long.");
+    }
+    else
+    {
+      window.alert("Invalid input! Please choose a number between 8 and 128.");
+      return generatePassword();
+    };
+  
+  let lowerChoice = window.confirm("Do you want lowercase letters in your password?");
+    if (lowerChoice)
+    {
+      true;
+    }
+    else
+    {
+      false;
+    }
+  let upperChoice = window.confirm("Do you want uppercase letters in your password?");
+    if (upperChoice)
+    {
+      true;
+    }
+    else
+    {
+      false;
+    }
+  let numberChoice = window.confirm("Do you want numbers in your password?");
+    if (numberChoice)
+    {
+      true;
+    }
+    else
+    {
+      false;
+    }
+  let symbolChoice = window.confirm("Do you want symbols in your password?");
+    if (symbolChoice)
+    {
+      true;
+    }
+    else
+    {
+      false;
+    }
 }
 
 // Get references to the #generate element
