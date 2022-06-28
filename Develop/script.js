cb13b0ec8c205d84c97ecdc478977d0f1c18dc4e
Victoria David <victoria@thedavids.us>
2022-06-28 01:05:56 -0400

    randomization functions added, not sure where to put them

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,63 @@
 // Assignment code here
 
+/*
+1. Click button to initialize prompts
+2. Choose password length
+3. Choose uppercase y/n, lowercase y/n, number y/n, special characters y/n
+4. Validate input
+5. Generate password
+6. Display password
+*/
+
+var password =
+[
+  {
+    randomLower: function()
+    {
+      var confirmLower = window.confirm("Do you want lowercase letters in your password?");
+      if (confirmLower)
+      {
+        var lowercase = "abcdefghijklmnopqrstuvwxyz";
+        return lowercase(Math.floor(Math.random() * lowercase.length));
+      }
+    }
+  },
+  {
+    randomUpper: function()
+    {
+      var confirmUpper = window.confirm("Do you want uppercase letters in your password?");
+      if (confirmUpper)
+      {
+        var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return uppercase(Math.floor(Math.random() * uppercase.length));
+      }
+    }
+  },
+  {
+    randomNumber: function()
+    {
+      var number = "0123456789";
+      return number(Math.floor(Math.random() * number.length));
+    }
+  },
+  {
+    randomSymbol: function()
+    {
+      var symbol = "!@#$%^&*=+./";
+      return symbol(Math.floor(Math.random() * symbol.length));
+    }
+  }
+]
+
+var generatePassword = function()
+{
+  var passwordLength = window.prompt("How many characters will your password have?")
+  if (passwordLength >= 8 && passwordLength <= 128)
+  {
+    passwordLength = parseInt(passwordLength);
+
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
